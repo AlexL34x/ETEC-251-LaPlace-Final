e7ac444123b4c406564c0b0a23ac83bb68a2b824
Eric <patt4179@gmail.com>
2017-12-04 13:58:43 -0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready( function() { 
+	$('.p20').animate({
+		width:'20%'
+	}, 1000);
+	
+	$('.p40').animate({
+		width:'40%'
+	}, 1000);
+	
+	$('.p70').animate({
+		width:'70%'
+	}, 1000);
+	
+	$('.p80').animate({
+		width:'80%'
+	}, 1000);
+ });
